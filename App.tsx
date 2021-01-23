9c0004604259a9e1ef406f732407e4829bca34f3
venancioavila <venanciodesouza@icloud.com>
2021-01-22 22:55:07 -0300

    first

---
+++ App.tsx
@@ -0,0 +1,118 @@
+import * as React from 'react';
+import {
+  StatusBar,
+  FlatList,
+  Image,
+  Animated,
+  Text,
+  View,
+  Dimensions,
+  StyleSheet,
+  TouchableOpacity,
+  Easing,
+  SafeAreaViewBase,
+  SafeAreaView,
+} from 'react-native';
+const {width, height} = Dimensions.get('screen');
+import faker from 'faker';
+
+faker.seed(10);
+const DATA = [...Array(30).keys()].map((_, i) => {
+  return {
+    key: faker.random.uuid(),
+    image: `https://randomuser.me/api/portraits/${faker.helpers.randomize([
+      'women',
+      'men',
+    ])}/${faker.random.number(60)}.jpg`,
+    name: faker.name.findName(),
+    jobTitle: faker.name.jobTitle(),
+    email: faker.internet.email(),
+  };
+});
+
+const BIG_IMG =
+  'https://external-preview.redd.it/GOkP8onbuyjGmN9Rc8Que5mw21CdSw6OuXpAKUuE6-4.jpg?auto=webp&s=2bc0e522d1f2fa887333286d557466b2be00fa5e';
+
+const SPACING = 20;
+const AVATAR_SIZE = 70;
+const ITEM_SIZE = AVATAR_SIZE + SPACING * 3;
+
+export default () => {
+  const scrollY = React.useRef(new Animated.Value(0)).current;
+
+  return (
+    <View style={{flex: 1, backgroundColor: '#fff'}}>
+      <StatusBar hidden />
+      <Image
+        source={{uri: BIG_IMG}}
+        style={StyleSheet.absoluteFillObject}
+        blurRadius={50}
+      />
+      <FlatList
+        data={DATA}
+        keyExtractor={(item) => item.key}
+        onScroll={Animated.event(
+          [{nativeEvent: {contentOffset: {y: scrollY}}}],
+          {useNativeDriver: false},
+        )}
+        contentContainerStyle={{
+          padding: SPACING,
+          paddingTop: StatusBar.currentHeight,
+        }}
+        renderItem={({item, index}) => {
+          const inputRange = [
+            -1,
+            0,
+            ITEM_SIZE * index,
+            ITEM_SIZE * (index + 2),
+          ];
+
+          const scale = scrollY.interpolate({
+            inputRange,
+            outputRange: [1, 1, 1, 0],
+          });
+
+          return (
+            <Animated.View
+              style={{
+                flexDirection: 'row',
+                padding: SPACING,
+                marginBottom: SPACING,
+                backgroundColor: 'rgba(255,255,255,0.8)',
+                borderRadius: 12,
+                shadowColor: '#000',
+                shadowOffset: {
+                  width: 0,
+                  height: 10,
+                },
+                shadowOpacity: 1,
+                shadowRadius: 20,
+                transform: [{scale}],
+              }}>
+              <Image
+                source={{uri: item.image}}
+                style={{
+                  width: AVATAR_SIZE,
+                  height: AVATAR_SIZE,
+                  borderRadius: AVATAR_SIZE / 2,
+                  marginRight: SPACING / 2,
+                }}
+              />
+              <View>
+                <Text style={{fontSize: 22, fontWeight: '700'}}>
+                  {item.name}
+                </Text>
+                <Text style={{fontSize: 16, opacity: 0.7, width: '100%'}}>
+                  {item.jobTitle}
+                </Text>
+                <Text style={{fontSize: 12, opacity: 0.8, color: '#8E44AD'}}>
+                  {item.email}
+                </Text>
+              </View>
+            </Animated.View>
+          );
+        }}
+      />
+    </View>
+  );
+};
